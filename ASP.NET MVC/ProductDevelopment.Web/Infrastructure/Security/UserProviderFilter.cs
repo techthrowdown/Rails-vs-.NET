b544786bfc023e2e8a832e918fdb0d4b4487a9fe
Ely Lucas <ely@meta-tek.net>
2011-08-21 14:39:20 -0600

    adding initial user stuff

---
+++ ASP.NET MVC/ProductDevelopment.Web/Infrastructure/Security/UserProviderFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using ProductDevelopment.Web.Models;
+
+namespace ProductDevelopment.Web.Infrastructure.Security
+{
+    public class UserProviderFilterAttribute : IAuthorizationFilter
+    {
+        private readonly IAuthentication _authentication;
+
+        public UserProviderFilterAttribute(IAuthentication authentication)
+        {
+            _authentication = authentication;
+        }
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var user = _authentication.CurrentUser() ?? new UserToken()
+            {
+                Admin = false,
+                Authenticated = false
+            };
+            filterContext.Controller.ViewBag.User = user;
+        }
+    }
+}
